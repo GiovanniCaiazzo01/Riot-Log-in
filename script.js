977b05d5adb6f214084c43a46b6c5d1ea20f335d
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-06-22 19:32:15 +0200

    chore: small chanes

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ const username = document.getElementsByTagName("input")[0];
 const password = document.getElementsByTagName("input")[1];
 const label_user = document.getElementsByTagName("label")[0];
 const label_password = document.getElementsByTagName("label")[1];
+const input_username = document.getElementById("username");
+const input_password = document.getElementById("password");
+
+let filled_name, filled_password;
 
 username.addEventListener("input", (e) => {
-  if (e.target.value) {
+  filled_name = e.target.value;
+
+  if (filled_name) {
     return label_user.classList.add("has-value");
   }
   return label_user.classList.remove("has-value");
 });
 
 password.addEventListener("input", (e) => {
-  if (e.target.value) {
+  filled_password = e.target.value;
+  if (filled_password) {
     return label_password.classList.add("has-value");
   }
   return label_password.classList.remove("has-value");
